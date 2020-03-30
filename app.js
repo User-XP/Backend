312ebc48eeeb61712d653ee588b9598406c7cd62
Shan Eapen Koshy <shaneapen@gmail.com>
2020-03-30 19:17:47 +0530

    cleanup

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
-var express=require("express"); 
-var bodyParser=require("body-parser"); 
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose'); 
-mongoose.connect('mongodb://localhost:27017/user' ,{useNewUrlParser: true}); 
-var db=mongoose.connection; 
-db.on('error', console.log.bind(console, "connection error")); 
-db.once('open', function(callback){ 
-	console.log("connection succeeded"); 
-}) 
+mongoose.connect('mongodb://localhost:27017/user', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
-var app=express() 
+var db = mongoose.connection;
 
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function (callback) {
+	console.log("connection succeeded");
+})
 
-app.use(bodyParser.json()); 
-app.use(express.static('public')); 
-app.use(bodyParser.urlencoded({ 
-	extended: true
-})); 
-
-app.post('/sign_up', function(req,res){ 
-	var name = req.body.name; 
-	var email =req.body.email; 
-    var pass = req.body.password; 
-    var company = req.body.companyname;
-	var phone =req.body.phone; 
-
-	var data = { 
-		"name": name, 
-		"email":email, 
-        "password":pass,
-        "company-name":company, 
-		"phone":phone 
-	} 
-db.collection('details').insertOne(data,function(err, collection){ 
-		if (err) throw err; 
-		console.log("Record inserted Successfully"); 
-			
-	}); 
-		
-	return res.redirect('signup_success.html'); 
-}) 
+var app = express()
 
 
-app.get('/',function(req,res){ 
-res.set({ 
-	'Access-control-Allow-Origin': '*'
-	}); 
-return res.redirect('registration-page.html'); 
-}).listen(3000) 
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/sign_up', function (req, res) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var pass = req.body.password;
+	var company = req.body.companyname;
+	var phone = req.body.phone;
+
+	var data = {
+		"name": name,
+		"email": email,
+		"password": pass,
+		"company-name": company,
+		"phone": phone
+	}
+	db.collection('details').insertOne(data, function (err, collection) {
+		if (err) throw err;
+		console.log("Record inserted Successfully");
+
+	});
+
+	return res.redirect('signup_success.html');
+})
+
+app.get('/',function(req,res){
+	return res.send("<p>Welcome! <a href='/signup'>Signup</a></p>")
+})
+
+app.get('/signup', function (req, res) {
+	res.set({
+		'Access-control-Allow-Origin': '*'
+	});
+	return res.redirect('registration-page.html');
+}).listen(3000)
 
 
-console.log("server listening at port 3000"); 
+console.log("server listening at port 3000");
